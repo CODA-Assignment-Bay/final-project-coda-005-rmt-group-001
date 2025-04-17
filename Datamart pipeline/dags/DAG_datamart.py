02e58c95566dc4ed4263118489cc4ce99fbf3a86
fahrizanrasyad <fahrizanrasyad@gmail.com>
2025-04-17 09:55:13 +0700

    ETL dag pt 1

--- Datamart pipeline/dags/DAG_datamart.py
+++ Datamart pipeline/dags/DAG_datamart.py
@@ -15,14 +15,14 @@ import os
 # Tambahkan folder scripts ke sys.path agar modul bisa diimport
 sys.path.append(os.path.join(os.path.dirname(__file__), "../scripts"))
 
-from extract import extract_data
-from transform import transform_data
+from extract import get_data_from_db
+from transform import pivot_and_rename, merge_pivot, process_summary, AQI_who
 from load import load_data
 
 
 default_args = {
     'owner': 'group-01',
-    'start_date': dt.datetime(2024, 11, 1),
+    'start_date': dt.datetime(2025, 4, 16),
     'retries': 1,
     'retry_delay': dt.timedelta(minutes=600),
 }
@@ -40,33 +40,62 @@ with DAG('P2FP_datamart',
          ) as dag:
     
     def extract_part():
-        measurement_item_df = extract_data('/opt/airflow/dags/measurement_item_info.csv')
-        measurement_info_df = extract_data('/opt/airflow/dags/measurement_info.csv')
-        measurement_station_df = extract_data('/opt/airflow/dags/measurement_station_info.csv')
-
-        measurement_item_df.to_csv(temp_csv_path+'extract_measurement_item.csv',index=False)
-        measurement_info_df.to_csv(temp_csv_path+'extract_measurement_info.csv',index=False)
-        measurement_station_df.to_csv(temp_csv_path+'extract_measurement_station.csv',index=False)
+        query = '''SELECT * FROM "Dim_Item";'''
+        df_item = get_data_from_db(query)
+        df_item.to_csv(temp_csv_path+'extract_df_item.csv',index=False)
         
+        query = '''SELECT * FROM "Dim_Station";'''
+        df_station = get_data_from_db(query)
+        df_station.to_csv(temp_csv_path+'extract_df_station.csv',index=False)
+
+        query = '''SELECT * FROM "Dim_Date" LIMIT 200;'''
+        df_date_sample = get_data_from_db(query)
+        df_date_sample.to_csv(temp_csv_path+'extract_df_date_sample.csv',index=False)
+
+        query = '''SELECT * FROM "Fact_Info" LIMIT 1000;'''
+        df_measurement_sample = get_data_from_db(query)
+        df_measurement_sample.to_csv(temp_csv_path+'extract_df_measurement_sample.csv',index=False)         
+        
+        #measurement_info_df.to_csv(temp_csv_path+'extract_measurement_info.csv',index=False)
+        #measurement_item_df = extract_data('/opt/airflow/dags/measurement_item_df.csv')
+
+        #df_item = get_data_from_db('/opt/airflow/dags/df_item.csv''SELECT * FROM "Dim_Item";''')) ???
+
        
     def transform_part():
-        measurement_info_df = pd.read_csv(temp_csv_path+'extract_measurement_info.csv')
 
-        measurement_info_df, dim_date = transform_data(measurement_info_df)
+        df_measurement_sample = pd.read_csv(temp_csv_path+'extract_df_measurement_sample.csv')
+        df_measurement_sample_piv = pivot_and_rename(df_measurement_sample)
+        df_measurement_sample_piv.to_csv(temp_csv_path+'df_measurement_sample_piv.csv',index=False) 
 
-        measurement_info_df.to_csv(temp_csv_path+'transform_measurement_info.csv',index=False)
-        dim_date.to_csv(temp_csv_path+'dim_date.csv',index=False)
+        df_measurement_sample_piv = pd.read_csv(temp_csv_path+'df_measurement_sample_piv.csv')
+        df_station = pd.read_csv(temp_csv_path+'extract_df_station.csv.csv')
+        df_merged = merge_pivot(df_measurement_sample_piv, df_station)
+        df_merged.to_csv(temp_csv_path+'df_merged.csv',index=False)
 
+        df_merged = pd.read_csv(temp_csv_path+'df_merged.csv')
+        df_summary = process_summary(df_merged)
+        df_summary.to_csv(temp_csv_path+'df_summary.csv',index=False)
+
+        df_summary = pd.read_csv(temp_csv_path+'df_summary.csv')
+        datamart = AQI_who(df_summary)
+        datamart.to_csv(temp_csv_path+'datamart.csv',index=False)
+
+        #measurement_info_df = pd.read_csv(temp_csv_path+'extract_measurement_info.csv')
+        #measurement_info_df, dim_date = transform_data(measurement_info_df)
+        #measurement_info_df.to_csv(temp_csv_path+'transform_measurement_info.csv',index=False)
+        #dim_date.to_csv(temp_csv_path+'dim_date.csv',index=False)
 
 
     def load_part():
-        measurement_info_df = pd.read_csv(temp_csv_path+'transform_measurement_info.csv')
-        measurement_item_df = pd.read_csv(temp_csv_path+'extract_measurement_item.csv')
-        measurement_station_df = pd.read_csv(temp_csv_path+'extract_measurement_station.csv')
-        dim_date = pd.read_csv(temp_csv_path+'dim_date.csv')
+        datamart = pd.read_csv(temp_csv_path+'datamart.csv')
+        load_data(datamart)
 
-        #load_data(measurement_station_df, measurement_item_df, measurement_info_df, dim_date, measurement_recap_df)
-        load_data(measurement_station_df, measurement_item_df, measurement_info_df, dim_date)
+        #measurement_info_df = pd.read_csv(temp_csv_path+'transform_measurement_info.csv')
+        #measurement_item_df = pd.read_csv(temp_csv_path+'extract_measurement_item.csv')
+        #measurement_station_df = pd.read_csv(temp_csv_path+'extract_measurement_station.csv')
+        #dim_date = pd.read_csv(temp_csv_path+'dim_date.csv')
+        #load_data(measurement_station_df, measurement_item_df, measurement_info_df, dim_date)
 
     extract = PythonOperator(
         task_id='extract',
